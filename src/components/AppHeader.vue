5796a664ad4f0a993a46f18ad341700452f0c49b
alexanderdeloecker <Alexander.de.loecker@student.ehb.be>
2025-12-23 22:14:14 +0100

    App header toevoeren

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+	<header style="padding: 24px 0">
+		<div class="container" style="display: flex; gap: 24px">
+			<router-link to="/">Home</router-link>
+			<router-link to="/add-win">Add win</router-link>
+			<router-link to="/login">Login</router-link>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+a {
+	text-decoration: none;
+	font-weight: 500;
+	color: inherit;
+}
+
+a.router-link-active {
+	text-decoration: underline;
+}
+</style>
